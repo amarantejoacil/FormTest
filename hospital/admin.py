f2a3b5046fd4380ccf6bb7b6e95ba686050c8422
Joacil Amarante <joacil.amarante@gmail.com>
2021-11-12 09:31:58 -0400

    criando estrutura para teste medico x paciente

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Medico, Paciente
 # Register your models here.
+
+
+
+admin.site.register(Medico)
+admin.site.register(Paciente)
--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Medico(models.Model):
+    numero_registro = models.IntegerField('N° de Registro')
+    nome = models.CharField('Nome', max_length=100)
+    data_nascimento = models.DateField('Data de nascimento')
+
+    def __str__(self):
+        return self.nome
+
+
+class Paciente(models.Model):
+    medico = models.ForeignKey('Medico', on_delete=models.PROTECT)
+    nome = models.CharField('Nome', max_length=100)
+    SEXO = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+    sexo = models.CharField('Sexo', choices=SEXO, max_length=1)
+
+    def __str__(self):
+        return 'Paciente: ' + self.nome + '| Medico: ' + str(self.medico)
+
+
+
